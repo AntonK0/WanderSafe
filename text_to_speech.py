83a9a5546de32661fb162a84decc32c0b7cef65c
Kaitei <kaiteinakayama2005@gmail.com>
2025-10-04 03:03:19 -0400

    text to speech

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,11 +2,12 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
+
 def speak(text, speed=140):
+    engine.setProperty('voice', 'com.apple.speech.synthesis.voice.alex')
     engine.setProperty('rate', speed)
     engine.say(text)
     engine.runAndWait()
     
-if __name__ == "__main__":
-    speak("Hello, Anton! Are u ok??", 180)
+
     
